9502de66fa7b89574bcc1513eb920b6e9e64eb5d
Loewy Malkovich <loewymalkov@gmail.com>
2019-08-14 16:17:35 -0700

    add front and back end ui for subtract, multiply and divide

--- js/scripts.js
+++ js/scripts.js
@@ -19,12 +19,41 @@ return num1/num2;
 // front end logic - user interface
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    event.preventDefault();
+
+  $("form#add").submit(function(addEvent) {
+    addEvent.preventDefault();
     var num1 = parseInt($("#add1").val());
     var num2 = parseInt($("#add2").val());
-    var result = add(num1, num2);
+    var addResult = add(num1, num2);
 
-    $("#output").text(result);
+    $("#output1").text(addResult);
   });
+
+  $("form#subtract").submit(function(subEvent) {
+    subEvent.preventDefault();
+    var num1 = parseInt($("#subtract1").val());
+    var num2 = parseInt($("#subtract2").val());
+    var subResult = subtract(num1, num2);
+    
+    $("#output2").text(subResult);
+  });
+  
+  $("form#multiply").submit(function(mulEvent) {
+    mulEvent.preventDefault();
+    var num1 = parseInt($("#multiply1").val());
+    var num2 = parseInt($("#multiply2").val());
+    var mulResult = multiply(num1, num2);
+    
+    $("#output3").text(mulResult);
+  });
+
+  $("form#divide").submit(function(divEvent) {
+    divEvent.preventDefault();
+    var num1 = parseInt($("#divide1").val());
+    var num2 = parseInt($("#divide2").val());
+    var divResult = divide(num1, num2);
+    
+    $("#output4").text(divResult);
+  });
+
 });
